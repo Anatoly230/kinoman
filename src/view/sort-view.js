309ca9b67e1f3f0fd07455a0f8e18ed58d0cfbd7
Anatoly <maximos225@mail.ru>
2023-04-22 11:05:28 +0300

    сменил локацию на desktop начал "рзбивать камни"

---
+++ src/view/sort-view.js
@@ -0,0 +1,25 @@
+import { createElement } from "../render"
+
+function createSortViewtemplate() {
+  return `<ul class="sort">
+    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+    <li><a href="#" class="sort__button">Sort by date</a></li>
+    <li><a href="#" class="sort__button">Sort by rating</a></li>
+  </ul>`
+}
+
+
+export default class SortView {
+  getTemplate() {
+    return createSortViewtemplate()
+  }
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+  }
+  removeElevent() {
+    this.element = null;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/main.js',
+
+  
+  entry: path.resolve(__dirname, './src/main.js'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
